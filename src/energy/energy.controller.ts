b39e9784246c7e0575a08ead192801e92edfc61c
Aditya Birangal <aditya.birangal@gmail.com>
2025-04-01 17:49:00 +0530

    Add error handling to Energy DID generation and for cobaltPUniqueId

--- src/energy/energy.controller.ts
+++ src/energy/energy.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Ip, Post, UseGuards } from '@nestjs/common';
 import { EnergyService } from './energy.service';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateDIDDto } from './create-did.dto';
+import { HttpErrorByCode } from '@nestjs/common/utils/http-error-by-code.util';
 
 @Controller('energy')
 export class EnergyController {
@@ -9,11 +10,21 @@ export class EnergyController {
 
   @Post()
   @UseGuards(AuthGuard('basic'))
-  getEnergyDid(@Ip() ip, @Body() createDIDDto: CreateDIDDto): any {
-    return this.energyService.getEnergyDid(
-      ip,
-      createDIDDto.issuedFor,
-      createDIDDto.cobaltPUniqueId,
-    );
+  async getEnergyDid(
+    @Ip() ip,
+    @Body() createDIDDto: CreateDIDDto,
+  ): Promise<any> {
+    try {
+      return await this.energyService.getEnergyDid(
+        ip,
+        createDIDDto.issuedFor,
+        createDIDDto.cobaltPUniqueId,
+      );
+    } catch (error) {
+      return Promise.reject({
+        statusCode: 500,
+        message: error.message,
+      });
+    }
   }
 }
--- src/energy/energy.service.ts
+++ src/energy/energy.service.ts
@@ -17,26 +17,37 @@ export class EnergyService {
     issuedFor: string,
     cobaltPUniqueId: string,
   ): Promise<any> {
-    const mySeedPhrase = generateSeedPhrase();
+    try {
+      const mySeedPhrase = generateSeedPhrase();
 
-    console.log('issuedFor: ', issuedFor);
-    // Eg. ed25519:2htvg4kn2Ps6NHiUaismbx6Zu1ZmnQm1Jx2kb2yyDedaawcPLZfcv4djc8BCxiutdHjPJZNUwoHyrmqwo5gYyEFv
-    // We need 32 bytes of secret key for creating a DID. we are taking 32 bytes from 8th to 40th byte.
-    const secretForDid = mySeedPhrase.secretKey.slice(8, 40);
+      console.log('issuedFor: ', issuedFor);
+      // Eg. ed25519:2htvg4kn2Ps6NHiUaismbx6Zu1ZmnQm1Jx2kb2yyDedaawcPLZfcv4djc8BCxiutdHjPJZNUwoHyrmqwo5gYyEFv
+      // We need 32 bytes of secret key for creating a DID. we are taking 32 bytes from 8th to 40th byte.
+      const secretForDid = mySeedPhrase.secretKey.slice(8, 40);
 
-    const { did, credential } = await identityCreation(secretForDid);
+      const { did, credential } = await identityCreation(secretForDid);
 
-    const newEnergyIdData = new this.energyIdDataModel({
-      did: did.toJSON(),
-      credential: JSON.stringify(credential),
-      privateInfo: mySeedPhrase,
-      requestIp: ip,
-      issuedFor: issuedFor,
-      cobaltPUniqueId: cobaltPUniqueId,
-    });
+      const newEnergyIdData = new this.energyIdDataModel({
+        did: did.toJSON(),
+        credential: JSON.stringify(credential),
+        privateInfo: mySeedPhrase,
+        requestIp: ip,
+        issuedFor: issuedFor,
+        cobaltPUniqueId: cobaltPUniqueId,
+      });
 
-    await newEnergyIdData.save();
+      await newEnergyIdData.save();
 
-    return did;
+      return did;
+    } catch (error) {
+      console.error('Error generating Energy DID:', error);
+
+      if ((error.errorResponse.code = 11000)) {
+        throw new Error(
+          'DID with this cobaltPUniqueId is already created. Please try again with a different cobaltPUniqueId.',
+        );
+      }
+      throw new Error('Failed to generate Energy DID. Please try again later.');
+    }
   }
 }
