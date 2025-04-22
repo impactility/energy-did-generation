ad46d2035855f2b16f4913f3c34bcecdd9c10160
Aditya Birangal <aditya.birangal@gmail.com>
2025-04-22 17:58:24 +0530

    v2 : Update IdentitiesController path to versioned API endpoint

--- src/identities/identities.controller.ts
+++ src/identities/identities.controller.ts
@@ -9,13 +9,12 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { IdentitiesService } from './identities.service';
-import { CreateIdentityDto } from './dto/create-identity.dto';
 import { UpdateIdentityDto } from './dto/update-identity.dto';
 import { CreateDIDDto } from 'src/energy/create-did.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { CreateCredentialDto } from './dto/create-credential.dto';
 
-@Controller('identities')
+@Controller('v2/identities')
 @UseGuards(AuthGuard('basic'))
 export class IdentitiesController {
   constructor(private readonly identitiesService: IdentitiesService) {}
--- src/identities/identities.module.ts
+++ src/identities/identities.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { IdentitiesService } from './identities.service';
 import { IdentitiesController } from './identities.controller';
-import { EnergyService } from 'src/energy/energy.service';
 import { EnergyModule } from 'src/energy/energy.module';
 
 @Module({
